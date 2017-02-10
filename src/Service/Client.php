6de9a1a400991891b545879c601142c6e68d48ff
Adam Jurek <adam.jurek@digitalvirgo.pl>
2017-02-10 13:35:13 +0100

    setup missing requests

--- src/Service/Client.php
+++ src/Service/Client.php
@@ -13,7 +13,8 @@ use GuzzleHttp\Stream\Stream;
  */
 class Client extends GuzzleClient {
 
-    const API_URL = 'http://mtserviceproxy.services.avantis.pl/';
+//    const API_URL = 'http://mtserviceproxy.services.avantis.pl/';
+    const API_URL = 'http://beta2:9080/mtsp/';
 
     /**
      * @var Client
@@ -124,7 +125,19 @@ class Client extends GuzzleClient {
 
         $options = [];
 
-        //@todo apply payload to request
+        switch ($method) {
+            case 'GET':
+                if (is_array($payload)) {
+                    $options['query'] = [$payload];
+                } else {
+                    $options['query'] = $payload;
+                }
+
+                break;
+            case 'POST':
+                $options['body'] = $payload;
+                break;
+        }
 
         $response = $this->send(
             $this->createRequest($method, $url, $options)
@@ -141,7 +154,87 @@ class Client extends GuzzleClient {
      * @return string
      */
     public function getServices() {
-        return $this->_request('/services');
+        return $this->_request('services');
     }
+
+    /**
+     * @param $serviceName Service name
+     * @return string
+     */
+    public function getService($serviceName) {
+        return $this->_request("services/{$serviceName}");
+    }
+
+    /**
+     * @param string $serviceName Service name
+     * @param null|string|\DateTime $from Optional date from filter
+     * @param null|string|\DateTime $to Optional date to filter
+     * @return string
+     */
+    public function getSubscriptions($serviceName, $from = null, $to = null) {
+
+        if ($from xor $to) {
+            throw new \Exception('Both dates are required');
+        }
+
+        $payload = [];
+
+        if ($from !== null) {
+            if (is_string($from)) {
+                $from = new \DateTime($from);
+            }
+
+            $payload['fromDate'] = $from->format('c');
+        }
+
+        if ($to !== null) {
+            if (is_string($to)) {
+                $to = new \DateTime($to);
+            }
+
+            $payload['toDate'] = $to->format('c');
+        }
+
+        return $this->_request("services/{$serviceName}/subscriptions", "GET", $payload);
+    }
+
+    public function getSubscription($serviceName, $subscriptionId) {
+        return $this->_request("services/{$serviceName}/subscriptions/{$subscriptionId}");
+    }
+
+    public function getBilledNumbers($serviceName, $subscriptionId, $id = null) {
+        $payload = [];
+
+        if ($id) {
+            $payload['id'] = $id;
+        }
+
+        return $this->_request("services/{$serviceName}/subscriptions/{$subscriptionId}/billing", "GET", $payload);
+    }
+
+    public function getSubscribers($serviceName, $operator = null) {
+
+        return $this->_request("services/{$serviceName}/subscribers/{$operator}");
+    }
+
+    public function addSubscription($serviceName, $scheduledTo, $message) {
+//        POST /services/{service_name}/subscriptions/.
+
+//        ALLOWED ASCII CHARACTERS IN TEXT CONTENTS
+//0 1 2 3 4 5 6 7 8 9 A B C D E F G H I J K L M N O P Q R S T U V W X Y Z a b c d
+//e f g h i j k l m n o p q r s t u v w x y z ą ć ę ł ń ó Ś ś ż ź Ą Ć Ę Ł Ń Ó Ś Ż
+//Ź ! @ # $ % & * ( ) - _ + = : ; ' " , . / \ < > ?     SP LF CR
+//
+//
+//        ADDITIONAL ALLOWED CHARACTERS IN TEXT CONTENTS FOR RUSSIAN OPERATORS
+//                                                           А Б В Г Д Е Ё Ж З И Й К Л М Н О П Р С Т У Ф Х Ц Ч Ш Щ Ъ Ы Ь Э Ю Я а б в г д е ё ж з
+//и й к л м н о п р с т у ф х ц ч ш щ ъ ы ь э ю я
+
+    }
+
+    public function updateSubscription($serviceName, $subscriptionId, $scheduledTo, $message) {
+//        PUT /services/{service name}/subscriptions/.
+    }
+
 
 }
